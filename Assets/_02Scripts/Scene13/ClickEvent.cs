bafad162f896e7cf925e6799fb4b54e91c35d93e
蔡广夺 <1064805848@qq.com>
2018-02-26 18:05:35 +0800

    AddScene

---
+++ Assets/_02Scripts/Scene13/ClickEvent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickEvent : MonoBehaviour
+{
+    public GameObject cube;
+    public GameObject image;
+
+    private void Start()
+    {
+        EventTriggerListener.Get(cube).onClick += OnCubeClick;
+        EventTriggerListener.Get(image).onClick += OnImageClick;
+    }
+
+    private void OnCubeClick(GameObject go)
+    {
+        if (go == cube)
+            Debug.Log("Cube");
+    }
+    private void OnImageClick(GameObject go)
+    {
+        if (go == image)
+            Debug.Log("Image");
+    }
+}
---
+++ Assets/_02Scripts/Scene13/PassEventClass.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class PassEventClass : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+{
+
+    //监听按下
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        PassEvent(eventData, ExecuteEvents.pointerDownHandler);
+    }
+
+    //监听抬起
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        PassEvent(eventData, ExecuteEvents.pointerUpHandler);
+    }
+
+    //监听点击
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        PassEvent(eventData, ExecuteEvents.submitHandler);
+        PassEvent(eventData, ExecuteEvents.pointerClickHandler);
+    }
+
+
+    //把事件透下去
+    public void PassEvent<T>(PointerEventData data, ExecuteEvents.EventFunction<T> function)
+        where T : IEventSystemHandler
+    {
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(data, results);
+        GameObject current = data.pointerCurrentRaycast.gameObject;
+        for (int i = 0; i < results.Count; i++)
+        {
+            if (current != results[i].gameObject)
+            {
+                ExecuteEvents.Execute(results[i].gameObject, data, function);
+                //RaycastAll后ugui会自己排序，如果你只想响应透下去的最近的一个响应，这里ExecuteEvents.Execute后直接break就行。
+            }
+        }
+    }
+
+
+}
